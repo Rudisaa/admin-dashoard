484fe112b7786fb3db99cb40594a21030ed3cac3
rudisaa <arguetaarudis@gmail.com>
2023-01-22 01:56:17 -0500

    mongoose setup

--- server/index.js
+++ server/index.js
@@ -18,3 +18,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 //routes -- react pages
+
+
+//mongoose setup
+// if the port in the env does not work use 9000 as the backup
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+}).catch((error) => console.log(`${error} did not connect`));
