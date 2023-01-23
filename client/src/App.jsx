b7f922017617c4b0c8ca16a811a2cdc8baa304e4
rudisaa <arguetaarudis@gmail.com>
2023-01-23 14:46:04 -0500

    added imports for mui and router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,27 @@
-import { useMemo } from 'react'
-import { BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
-import Random from './components/random';
-import NavBar from './components/NavBar';
-import './app.css';
+import { CssBaseline, CSSBaseLine, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { themeSettings } from './theme';
+
+
+
 
 
 function App() {
 
-  
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode), [mode]))
 
   return (
     <div className="App">
       <BrowserRouter>
-        
+        <ThemeProvider theme={ theme }> 
+        <CSSBaseLine />
+        <Route element={<Layout />}>
+          
+        </Route>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   )
