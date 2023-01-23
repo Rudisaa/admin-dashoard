dd606cb9126e53949e84cac0f218c27b3de4f6c2
rudisaa <arguetaarudis@gmail.com>
2023-01-23 00:05:32 -0500

    removed duplicate reactdom import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { setupListeners} from '@reduxjs/toolkit/query';
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware)
+});
+setupListeners(store.dispatch);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
