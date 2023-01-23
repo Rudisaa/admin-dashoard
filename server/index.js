17fef49024cef8b69987b58ab13bac03d8a18e24
rudisaa <arguetaarudis@gmail.com>
2023-01-22 21:30:53 -0500

    formatted a line of code

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json);
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 //routes -- react pages
