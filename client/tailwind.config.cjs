bac19cb1ae35dc5f7f5f8456eff8a4dbf025b724
rudisaa <arguetaarudis@gmail.com>
2023-01-23 01:58:23 -0500

    added daisyui configs

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,4 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark'],
+    styled: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark',
+  }
 };
