5811f6474d01f51c89e482c7d5d56d6b0b34720d
rudisaa <arguetaarudis@gmail.com>
2023-01-23 00:10:05 -0500

    added code to api

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,1 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+//api state
+export const api = createApi({
+
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+    reducerPath: 'adminApi',
+    tagTypes: [],
+    endpoints: (build) => ({
+
+    })
+});
+
+export const {
+
+} = api;
