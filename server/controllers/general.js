a5a70519d5b484c0a16ab7e3ab55b71db7eb85ee
rudisaa <arguetaarudis@gmail.com>
2023-01-22 23:24:48 -0500

    removed duplicate import

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,7 +1,6 @@
 import User from "../models/User.js";
 import OverallStat from "../models/OverallStat.js";
 import Transaction from "../models/Transaction.js";
-import OverallStat from "../models/OverallStat.js";
 
 export const getUser = async (req, res) => {
 
