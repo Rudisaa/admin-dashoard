4e4f19764ab7a4fb98729f628f674699f7dc07f7
rudisaa <arguetaarudis@gmail.com>
2023-01-23 01:59:47 -0500

    added provided and imported globalreducer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import App from './App';
 import './index.css';
-import { configureStore } from '@reduxjs/toolkit';
 import ReactDOM from 'react-dom/client';
+import globalReducer from './state'
 import { Provider } from 'react-redux';
 import { setupListeners} from '@reduxjs/toolkit/query';
 import { api } from './state/api'
+import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
   reducer: {
@@ -15,7 +16,6 @@ const store = configureStore({
   middleware: (getDefault) => getDefault().concat(api.middleware)
 });
 setupListeners(store.dispatch);
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
