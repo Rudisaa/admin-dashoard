83560d06e092d7d64d174da4901eae7050654228
rudisaa <arguetaarudis@gmail.com>
2023-01-23 14:37:35 -0500

    removed nav bar and random test component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { useMemo } from 'react'
 import { BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import Random from './components/random';
+import NavBar from './components/NavBar';
+import './app.css';
 
 
 function App() {
 
+  
+
   return (
     <div className="App">
       <BrowserRouter>
-      hello
+        
       </BrowserRouter>
     </div>
   )
