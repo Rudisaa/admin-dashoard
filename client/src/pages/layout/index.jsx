7466b277d1901075c47661119d69af5f22516e29
rudisaa <arguetaarudis@gmail.com>
2023-01-26 13:47:16 -0500

    added navbar to layout

--- client/src/pages/layout/index.jsx
+++ client/src/pages/layout/index.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavBar from '../../components/NavBar';
+import { Box, useMediaQuery } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+
 
 const Layout = () => {
+
+  const isNonMobile = useMediaQuery('(min-width: 600px)');
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
+
+
   return (
-    <div>Layout</div>
+    <Box 
+      display={isNonMobile ? 'flex' : 'block'}
+      width='100%'
+      height='100%'
+    >
+      <Box flexGrow={1}>
+        <NavBar 
+          user={ data || {} } 
+          isSidebarOpen={ isSideBarOpen }
+          setIsSidebarOpen={ setIsSideBarOpen }
+        >
+        </NavBar>
+        <Outlet/>
+      </Box>
+    </Box>
   )
 }
 
