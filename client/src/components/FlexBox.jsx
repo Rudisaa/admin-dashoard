66dd43b5543684d72102a5393821cf193ebcedbd
rudisaa <arguetaarudis@gmail.com>
2023-01-26 13:46:55 -0500

    created flexbox component

---
+++ client/src/components/FlexBox.jsx
@@ -0,0 +1,9 @@
+import { styled, Box } from '@mui/system';
+
+const FlexBox = styled(Box)({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+});
+
+export default FlexBox;
